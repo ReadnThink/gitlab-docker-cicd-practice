89bdfeb66fbdbb9dcb2665b4969337a5f05c9ff4
solbae <sol4854@naver.com>
2022-12-13 17:02:34 +0900

    message 변경

--- src/main/java/com/prac/dockergitlab/controller/MessageController.java
+++ src/main/java/com/prac/dockergitlab/controller/MessageController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     @GetMapping("")
     public ResponseEntity<String> message() {
-        return ResponseEntity.ok().body("bye");
+        return ResponseEntity.ok().body("bye 이론");
     }
 }
