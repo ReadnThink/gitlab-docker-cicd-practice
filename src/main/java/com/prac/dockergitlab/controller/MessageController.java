fba8a32ad3432b97833a0754ed0326113a65e0b8
solbae <sol4854@naver.com>
2022-12-13 15:36:58 +0900

    hello -> bye

--- src/main/java/com/prac/dockergitlab/controller/MessageController.java
+++ src/main/java/com/prac/dockergitlab/controller/MessageController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     @GetMapping("")
     public ResponseEntity<String> message() {
-        return ResponseEntity.ok().body("Hello");
+        return ResponseEntity.ok().body("bye");
     }
 }
