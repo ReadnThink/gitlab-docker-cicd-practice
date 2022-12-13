77694b19c65f5d7b96eaae212e496acd8cb24168
solbae <sol4854@naver.com>
2022-12-13 14:52:17 +0900

    first commit

---
+++ src/main/java/com/prac/dockergitlab/DockerGitlabPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.prac.dockergitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGitlabPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerGitlabPracticeApplication.class, args);
+    }
+
+}
